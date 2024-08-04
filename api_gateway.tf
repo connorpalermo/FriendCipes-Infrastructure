91015d5bb8d177301153a5c6417ce624cf115262
connorpalermo <cjpalermo1996@gmail.com>
2024-08-04 10:22:20 -0400

    try adding depends on

--- api_gateway.tf
+++ api_gateway.tf
@@ -14,6 +14,7 @@ resource "aws_api_gateway_method" "proxy" {
   resource_id   = aws_api_gateway_resource.proxy.id
   http_method   = "ANY"
   authorization = "NONE"
+  depends_on = [aws_api_gateway_resource.proxy]
 }
 
 resource "aws_api_gateway_integration" "lambda" {
@@ -43,10 +44,10 @@ resource "aws_api_gateway_integration" "lambda_root" {
   uri                     = aws_lambda_function.friendcipes-core-lambda.invoke_arn
 }
 
-resource "aws_api_gateway_deployment" "example" {
+resource "aws_api_gateway_deployment" "friendcipes-core-api" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    aws_api_gateway_integration.lambda,
+    aws_api_gateway_integration.lambda_root,
   ]
 
   rest_api_id = aws_api_gateway_rest_api.friendcipes-core-api.id
