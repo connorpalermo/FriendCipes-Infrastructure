8b770110fd25f56e8dbb715e54bd9f5fd55e4d46
connorpalermo <cjpalermo1996@gmail.com>
2024-08-04 10:30:23 -0400

    add base_url output

--- output.tf
+++ output.tf
@@ -1,5 +1,9 @@
 
-output "address" {
+output "db_address" {
   value       = aws_db_instance.friendcipesdb.address
   description = "Address of RDS instance"
+}
+
+output "api_gw_base_url" {
+  value = aws_api_gateway_deployment.friendcipes-core-api.invoke_url
 }
