44961c182ac64a102108aa3eedf07d87be81ee3e
connorpalermo <cjpalermo1996@gmail.com>
2024-08-07 20:08:38 -0400

    fix lambda var

--- variables.tf
+++ variables.tf
@@ -5,6 +5,10 @@ variable "db_pw" {
   type = string
 }
 
+variable "lambda_version" {
+  type = string
+}
+
 variable "db_engine" {
   type = string
   description = "Database Engine for RDS Instance"
@@ -15,12 +19,6 @@ variable "db_engine_version" {
   type = string
   description = "Engine Version for RDS Instance"
   default = "14"
-}
-
-variable "lambda_version" {
-  type = string
-  description = "Lambda Version"
-  default = "1.0.0"
 }
 
 
